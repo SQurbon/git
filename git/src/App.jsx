ec1d447222ff2aafee840c10406521ca0cc5221c
“SQurbon” <qurbonsultonqulov466@gmail.com>
2025-04-17 15:04:54 +0500

    fix

--- git/src/App.jsx
+++ git/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      <div> 
+      
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+   
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
